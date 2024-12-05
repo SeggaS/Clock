1434d0b13f892e7492f6eefcd334de730f7eb66e
SeggaS <134197516+SeggaS@users.noreply.github.com>
2024-12-05 02:13:08 +0200

    app.py

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ app = Flask(__name__)
 @app.route("/")
 def show_time():
     current_time = datetime.datetime.now().strftime("%H:%M:%S")
-    return f"<h1>Текущее время: {current_time}</h1>"
+    return f"<h1>Поточний час: {current_time}</h1>"
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Получить порт из переменной окружения
-    app.run(host="0.0.0.0", port=port)        # Слушать на всех интерфейсах
+    port = int(os.environ.get("PORT", 5000))  
+    app.run(host="0.0.0.0", port=port)        
